ec4a6e01b88d10e4468e9ed79588bc5cd35995b7
PanSzczerba <26200369+PanSzczerba@users.noreply.github.com>
2018-07-27 23:34:41 +0200

    Generic crc implementation

---
+++ src/main/crc/crc.h
@@ -0,0 +1,13 @@
+#ifndef MEXT2_CRC
+#define MEXT2_CRC
+#include <stdint.h>
+#include <stddef.h>
+
+#define CRC7_POLYNOMIAL 0x09
+#define CRC32_POLYNOMIAL 0x04C11DB7
+
+#define crc7(data, data_length) mext2_generic_crc((data), (data_length), CRC7_POLYNOMIAL, 7)
+#define crc32(data, data_length) mext2_generic_crc((data), (data_length), CRC32_POLYNOMIAL, 32)
+
+uint32_t mext2_generic_crc(uint8_t* data, size_t data_length, uint32_t polynomial, uint8_t polynomial_length);
+#endif
